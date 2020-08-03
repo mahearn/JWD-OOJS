7cb2ef7fffb60b1f20a05c84c0e90aa3e0cc323e
Michael Ahearn <mahearnpad@gmail.com>
2020-08-03 16:48:27 +1000

    changes made during zoom session

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+class Task {
+  constructor(id, name, description, assignedTo, dueDate, status) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.assignedTo = assignedTo;
+    this.dueDate = dueDate;
+    this.status = status;
+  }
+
+  addTask() {
+    //return a JS object
+    return `a JS object`;
+  }
+  clearForm() {}
+}
+
+const task1 = new Task(
+  1,
+  'a new task',
+  'a new task what I just made',
+  '03-08-2020',
+  'TODO'
+);
+
+task1.addTask();
+
+const taskList = [
+  { id: 1, name: 'a name', description: 'blah blah blah' },
+  { id: 2, name: 'a name', description: 'blah blah blah' },
+];
+
 class Book {
   constructor(title, author, year) {
     this.title = title;
